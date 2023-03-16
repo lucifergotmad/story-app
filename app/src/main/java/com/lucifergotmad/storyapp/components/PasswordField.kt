4441b674592909d5a08747240fa4d2b8fa995696
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-03-17 01:39:22 +0700

    feat:[minor] add custom password component

--- app/src/main/java/com/lucifergotmad/storyapp/components/PasswordField.kt
+++ app/src/main/java/com/lucifergotmad/storyapp/components/PasswordField.kt
@@ -2,13 +2,12 @@ package com.lucifergotmad.storyapp.components
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
 
 class PasswordField : AppCompatEditText {
-    private lateinit var showButtonImage: Drawable
-
     constructor(context: Context) : super(context) {
         init()
     }
@@ -30,7 +29,25 @@ class PasswordField : AppCompatEditText {
     }
 
     private fun init() {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Do nothing.
+            }
 
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.isEmpty() || s.length < 8) {
+                    error = "Password length must be more than 8 characters"
+                    setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Do nothing.
+            }
+        })
     }
+
 
 }
