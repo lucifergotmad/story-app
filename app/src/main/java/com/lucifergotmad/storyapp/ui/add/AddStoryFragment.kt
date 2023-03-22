e2de2076b95a86a0eb1aa5f599be53237d82db61
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-03-22 13:28:41 +0700

    feat:[minor] setup camera fragment

--- app/src/main/java/com/lucifergotmad/storyapp/ui/add/AddStoryFragment.kt
+++ app/src/main/java/com/lucifergotmad/storyapp/ui/add/AddStoryFragment.kt
@@ -2,14 +2,18 @@ package com.lucifergotmad.storyapp.ui.add
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsets
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.lucifergotmad.storyapp.databinding.FragmentAddStoryBinding
 
 class AddStoryFragment : Fragment() {
@@ -45,9 +49,29 @@ class AddStoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupView()
+
         if (!allPermissionsGranted()) {
             startPermissionRequest()
         }
+
+        binding.btnCamera.setOnClickListener {
+            val toCameraFragment =
+                AddStoryFragmentDirections.actionAddStoryFragmentToCameraFragment()
+            findNavController().navigate(toCameraFragment)
+        }
+    }
+
+    private fun setupView() {
+        val window = (requireActivity() as AppCompatActivity).window
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            if (window.insetsController != null) {
+                window.insetsController
+                    ?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            }
+        }
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
--- app/src/main/java/com/lucifergotmad/storyapp/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/lucifergotmad/storyapp/ui/camera/CameraFragment.kt
@@ -1,14 +1,22 @@
 package com.lucifergotmad.storyapp.ui.camera
 
+import android.os.Build
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.lucifergotmad.storyapp.databinding.FragmentCameraBinding
 
 class CameraFragment : Fragment() {
     private lateinit var binding: FragmentCameraBinding
+    private var imageCapture: ImageCapture? = null
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +24,62 @@ class CameraFragment : Fragment() {
     ): View {
         binding = FragmentCameraBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupView()
+        startCamera()
+    }
+
+    private fun setupView() {
+        val window = (requireActivity() as AppCompatActivity).window
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            if (window.insetsController != null) {
+                window.insetsController
+                    ?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                window.insetsController?.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+        }
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    private fun startCamera() {
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            } catch (exc: Exception) {
+                Toast.makeText(
+                    requireContext(),
+                    "Failed showing camera.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 }
