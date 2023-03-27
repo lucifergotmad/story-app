5fb1fb96f2d484831b919885227adbae798883d4
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-03-27 09:27:59 +0700

    chore:[minor] add fragmentmapsbinding

--- app/src/main/java/com/lucifergotmad/storyapp/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/lucifergotmad/storyapp/ui/maps/MapsFragment.kt
@@ -8,14 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.lucifergotmad.storyapp.R
+import com.lucifergotmad.storyapp.databinding.FragmentMapsBinding
 
 class MapsFragment : Fragment() {
+    private lateinit var binding: FragmentMapsBinding
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -36,8 +37,9 @@ class MapsFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+    ): View {
+        binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
